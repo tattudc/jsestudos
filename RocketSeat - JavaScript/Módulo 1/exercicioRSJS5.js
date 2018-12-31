b96cb385894252011c1e0f444dc6cb5ea3a75204
tattudc <tarcisiodantasrn@gmail.com>
2018-12-31 14:41:40 -0200

    Organizar e Novos Ups
    Novos Ups terminando Módulo 1 do Curso de JS da RocketSeat e organizando as pastas

---
+++ RocketSeat - JavaScript/Módulo 1/exercicioRSJS5.js
@@ -0,0 +1,27 @@
+function exibeHabilidades(usuarios) {
+   for (usuario of usuarios) {
+     console.log(
+       "O " +
+         usuario.nome +
+         " possui as habilidades: " +
+         usuario.habilidades.join(", ")
+     );
+   }
+ }
+
+var usuarios = [
+    {
+    nome: "Diego",
+    habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+    nome: "Gabriel",
+    habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+   ];
+
+   exibeHabilidades(usuarios);
+
+
+//O Diego possui as habilidades: Javascript, ReactJS, Redux
+//O Gabriel possui as habilidades: VueJS, Ruby on Rails, Elixir
