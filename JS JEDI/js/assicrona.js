91e97294dd99f823fff500cb86f3f3faf06e8617
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-04-02 20:32:12 -0200

    Aulas de hj de mestre Jedi JS

---
+++ JS JEDI/js/assicrona.js
@@ -0,0 +1,8 @@
+console.log("Começando programação sícrona");
+
+//função timer programação assícrona
+setTimeout(function(){
+	console.log("Executando programação assícrona");
+}, 2000);
+
+console.log("Termino da programação sicrona");
---
+++ JS JEDI/js/sicrono.js
@@ -0,0 +1,9 @@
+var curso = "Formação mestre Jedi JS";
+
+console.log("Olá seja bem vindo ao curso " + curso);
+
+for(let index = 0; index < 10; index++){
+	console.log(index);
+	};
+
+console.log("Este curso é 10!");
